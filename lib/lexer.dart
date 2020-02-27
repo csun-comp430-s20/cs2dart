1dae1e430877a0923b805410545ca2e384a742ae
massivelivefun <matthewlancefuller@gmail.com>
2020-02-26 22:42:53 -0800

    Changed library collections to include more files

--- lib/lexer.dart
+++ lib/lexer.dart
@@ -1,4 +1,5 @@
 library lexer;
 
-export 'src/lexer/tokenizer.dart';
-export 'src/lexer/token_assist.dart';
+export 'src/lexer/lexer.dart';
+export 'src/lexer/lexer_assist.dart';
+export 'playground/token_assist.dart';
