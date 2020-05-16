dfb65a5f59d202c69c49a52d5252fa9b4abde6bb
DCiliberto <dominicciliberto@gmail.com>
2020-05-16 16:42:54 -0700

    type generation code finished

--- lib/src/code_generator/code_generator.dart
+++ lib/src/code_generator/code_generator.dart
@@ -5,7 +5,11 @@ import 'package:cs2dart/src/classes/variants/variants/MethodDeclaration.dart';
 import 'package:cs2dart/src/namespaces/namespace.dart';
 import 'package:cs2dart/src/statements/variants/DeclarationStatementVariants/constant_declaration.dart';
 import 'package:cs2dart/src/statements/variants/DeclarationStatementVariants/local_variable_declaration.dart';
-
+import 'package:cs2dart/src/tokens/token.dart';
+import 'package:cs2dart/src/types/type.dart';
+import 'package:cs2dart/src/types/variants/value_type.dart';
+import 'package:cs2dart/src/types/variants/variants/floating_point_type.dart';
+import 'package:cs2dart/src/types/variants/variants/integral_type.dart';
 class ClassGenerator {
   ClassGenerator(this._namespace);
     
@@ -69,5 +73,56 @@ class ClassGenerator {
       output += '}';
       return output;
     }
+
+  String genConstDeclarationCode(ConstantDeclaration value) {
+    var output = '';
+    output += value.value[0].value + ' ';
+    output += genTypeCode(value.value[1]) + ' ';
+    output += value.value[2] + ' ';
+    if(value.value.length > 3){
+      output += value.value[3].value + ' ';
+      output += genExpressionCode(value.value[4]) + ' ';
+    }
+    return output;
+  }
+
+  String genTypeCode(Type input){
+    var output = '';
+    if(input.runtimeType.toString() == 'ValueType'){
+      output += genValueTypeCode(input.value[0]) + ' ';
+    }
+    else{
+      output += genReferenceTypeCode(input.value[0]) + ' ';
+    }
+    return output;
+  }
+
+  String genValueTypeCode(ValueType input){
+    var output = '';
+    if(input.value[0] is Token){
+      output += input.value[0].value + ' ';
+      return output;
+    }
+    else if(input.value[0] is FloatingPointType){
+      output += genFloatingPointType(input.value[0]) + ' ';
+      return output;
+    }
+    else{
+      output += genIntegralType(input.value[0]) + ' ';
+      return output;
+    }
+  }
+
+  String genFloatingPointType(FloatingPointType input){
+    var output = '';
+    output += input.value[0].value + ' ';
+    return output;
+  }
+
+  String genIntegralType(IntegralType input){
+    var output = '';
+    output += input.value[0].value + ' ';
+    return output;
+  }
   
 }
